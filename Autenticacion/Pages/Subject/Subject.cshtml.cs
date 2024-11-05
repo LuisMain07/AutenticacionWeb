7301efa6fb9af10d45b05498cd5d43a026ba3061
LuisMain07 <bdmainguez00@gmail.com>
2024-11-05 11:31:06 -0500

    Primer commit Layout

---
+++ Autenticacion/Pages/Subject/Subject.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Autenticacion.Pages.Subjet
+{
+    public class SubjectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
